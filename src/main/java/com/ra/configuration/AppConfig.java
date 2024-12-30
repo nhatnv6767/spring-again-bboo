8926e0bf91eabff03cab102be9887830fd62b7e1
bhnone <>
2024-12-30 15:59:45 +0900

    l9 cors : third way

--- src/main/java/com/ra/configuration/AppConfig.java
+++ src/main/java/com/ra/configuration/AppConfig.java
@@ -1,12 +1,19 @@
 package com.ra.configuration;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
+
+    // first way need to implement WebMvcConfigurer
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**")
@@ -17,18 +24,38 @@ public class AppConfig {
 //        ;
 //    }
 
+    // second way need dont need to implement WebMvcConfigurer
+//    @Bean
+//    public WebMvcConfigurer corsFilter() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowCredentials(true)
+//                        .allowedOrigins("http://localhost:5173", "*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "*")
+//                        .allowedHeaders("*")
+//                ;
+//            }
+//        };
+//    }
+
     @Bean
-    public WebMvcConfigurer corsFilter() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowCredentials(true)
-                        .allowedOrigins("http://localhost:5173", "*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "*")
-                        .allowedHeaders("*")
-                ;
-            }
-        };
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        // accept language, content type
+        config.addAllowedHeader("*");
+        // hoac la chi dinh cu the, vi du : /user
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        // khoi tao dau tien
+//        bean.setOrder(0);
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+
     }
 }

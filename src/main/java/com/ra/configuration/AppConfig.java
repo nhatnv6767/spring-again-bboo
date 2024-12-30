5a911de13654847bfa297d858fa630cacbb32139
bhnone <>
2024-12-30 15:53:26 +0900

    l9 cors : second way

--- src/main/java/com/ra/configuration/AppConfig.java
+++ src/main/java/com/ra/configuration/AppConfig.java
@@ -1,18 +1,34 @@
 package com.ra.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:5173", "*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-        ;
+public class AppConfig {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins("http://localhost:5173", "*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//        ;
+//    }
+
+    @Bean
+    public WebMvcConfigurer corsFilter() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins("http://localhost:5173", "*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "*")
+                        .allowedHeaders("*")
+                ;
+            }
+        };
     }
 }

a1c61e84770d7903b31a1a101773068eedf6dd7a
bhnone <>
2024-12-30 22:39:14 +0900

    l11 build app environment !prod

--- src/main/java/com/ra/configuration/OpenApiConfig.java
+++ src/main/java/com/ra/configuration/OpenApiConfig.java
@@ -1,20 +1,20 @@
 package com.ra.configuration;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
 @Configuration
+// build khi chạy ở môi trường khác prod
+@Profile("!prod")
 public class OpenApiConfig {
 
     @Bean

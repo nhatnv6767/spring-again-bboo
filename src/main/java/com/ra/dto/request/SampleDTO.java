8b31957225a5c069831c0a7df24b36443148da1c
bhnone <>
2024-12-30 15:44:27 +0900

    l9 cors start

--- src/main/java/com/ra/dto/request/SampleDTO.java
+++ src/main/java/com/ra/dto/request/SampleDTO.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.Serializable;
 
 @AllArgsConstructor(access = AccessLevel.NONE)
-// best practice in DTO is to use only @Getter, in response DTO we can use @Builder
+// best practice in DTO is to use only @Getter, in response DTO we can use @Builder...
 public class SampleDTO implements Serializable {
     private Integer id;
     private String name;

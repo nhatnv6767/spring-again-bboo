778e05eb2b7bd2c322446040ceb1463e7d1c7b6c
bhnone <>
2024-12-30 21:32:42 +0900

    l10 API document start JWT token include

--- src/main/java/com/ra/configuration/OpenApiConfig.java
+++ src/main/java/com/ra/configuration/OpenApiConfig.java
@@ -1,9 +1,14 @@
 package com.ra.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,16 +18,42 @@ import java.util.List;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(
+            @Value("${open.api.title}") String title,
+            @Value("${open.api.version}") String version,
+            @Value("${open.api.description}") String description,
+            @Value("${open.api.license}") String license,
+            @Value("${open.api.server.url}") String serverUrl,
+            @Value("${open.api.server.description}") String serverDescription
+    ) {
         return new OpenAPI().info(
                 new Info()
-                        .title("API-services document")
-                        .version("v1.0.0")
-                        .description("Description for API services")
-                        .license(new License().name("API License").url("http://domain.vn/license"))
+                        .title(title)
+                        .version(version)
+                        .description(description)
+                        .license(new License().name(license).url("http://domain.vn/license"))
         ).servers(List.of(
-                new Server().url("http://192.168.1.202:8080").description("Production server"),
+                new Server().url(serverUrl).description(serverDescription),
                 new Server().url("http://localhost:8080").description("Development server")
-        ));
+        )).components(
+                // Mỗi khi gửi request lên server, cần phải có token JWT để xác thực (kiểu như tích hợp 1 lần, những lần sau được tích hợp sẵn vào luôn)
+                new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+        ).security(List.of(new SecurityRequirement().addList("bearerAuth")))
+                ;
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("api-service")
+                .packagesToScan("com.ra.controller")
+                .build();
     }
 }

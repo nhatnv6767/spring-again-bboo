c38252a2ea07302b911ed2e143e5e7994db3b8a1
bhnone <>
2024-12-31 22:23:35 +0900

    l15 Spring JPA customize prepare query best practice for query

--- src/main/java/com/ra/repository/SearchRepository.java
+++ src/main/java/com/ra/repository/SearchRepository.java
@@ -15,7 +15,8 @@ public class SearchRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public PageResponse<?> getAllUsersWithSortByColumnAndSearch(int pageNo, int pageSize, String search, String sortBy) {
+    public PageResponse<?> getAllUsersWithSortByColumnAndSearch(int pageNo, int pageSize, String search,
+            String sortBy) {
         // query list user
         StringBuilder sqlQuery = new StringBuilder("Select u from User u where 1=1");
         if (StringUtils.hasLength(search)) {
@@ -28,9 +29,19 @@ public class SearchRepository {
         selectQuery.setMaxResults(pageSize);
 
         if (StringUtils.hasLength(search)) {
-            selectQuery.setParameter("firstName", "%" + search + "%");
-            selectQuery.setParameter("lastName", "%" + search + "%");
-            selectQuery.setParameter("email", "%" + search + "%");
+            // String.format("%%%s%%", search) được sử dụng để tạo pattern cho câu lệnh LIKE
+            // trong SQL
+            // Trong đó:
+            // %% - Đại diện cho ký tự % trong chuỗi kết quả vì % là ký tự đặc biệt trong
+            // String.format()
+            // %s - Placeholder sẽ được thay thế bằng giá trị của tham số search
+            // Ví dụ: Nếu search = "john"
+            // Kết quả: String.format("%%%s%%", "john") = "%john%"
+            // Điều này cho phép tìm kiếm các bản ghi có chứa "john" ở bất kỳ vị trí nào
+            // VD: "john doe", "johnny", "mr john" đều sẽ được tìm thấy
+            selectQuery.setParameter("firstName", String.format("%%%s%%", search));
+            selectQuery.setParameter("lastName", String.format("%%%s%%", search));
+            selectQuery.setParameter("email", String.format("%%%s%%", search));
         }
 
         List users = selectQuery.getResultList();

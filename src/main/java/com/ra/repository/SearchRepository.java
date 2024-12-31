98f0f2a1762276394143225f28d7887eaf375b6b
bhnone <>
2024-12-31 22:20:47 +0900

    l15 Spring JPA customize prepare query bad smell "search like"

--- src/main/java/com/ra/repository/SearchRepository.java
+++ src/main/java/com/ra/repository/SearchRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -16,11 +17,24 @@ public class SearchRepository {
 
     public PageResponse<?> getAllUsersWithSortByColumnAndSearch(int pageNo, int pageSize, String search, String sortBy) {
         // query list user
-        StringBuilder sqlQuery = new StringBuilder("Select u from User u");
+        StringBuilder sqlQuery = new StringBuilder("Select u from User u where 1=1");
+        if (StringUtils.hasLength(search)) {
+            sqlQuery.append(" and lower(u.firstName) like lower(:firstName)");
+            sqlQuery.append(" or lower(u.lastName) like lower(:lastName)");
+            sqlQuery.append(" or lower(u.email) like lower(:email)");
+        }
         Query selectQuery = entityManager.createQuery(sqlQuery.toString());
         selectQuery.setFirstResult(pageNo);
         selectQuery.setMaxResults(pageSize);
+
+        if (StringUtils.hasLength(search)) {
+            selectQuery.setParameter("firstName", "%" + search + "%");
+            selectQuery.setParameter("lastName", "%" + search + "%");
+            selectQuery.setParameter("email", "%" + search + "%");
+        }
+
         List users = selectQuery.getResultList();
+        System.out.println("users = " + users);
 
         // query num of record
         return null;

bab23eba0ef44b6762f44b294db3edc759c7350c
bhnone <>
2024-12-31 23:50:46 +0900

    l16 JPA - Criteria

--- src/main/java/com/ra/dto/response/UserDetailResponse.java
+++ src/main/java/com/ra/dto/response/UserDetailResponse.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 @Builder
 @Getter
-//@AllArgsConstructor
+@AllArgsConstructor
 public class UserDetailResponse implements Serializable {
     private Long id;
 
@@ -42,15 +42,4 @@ public class UserDetailResponse implements Serializable {
         this.username = username;
     }
 
-    public UserDetailResponse(Long id, String firstName, String lastName, String email, String phone, Date dateOfBirth, Gender gender, String username, UserStatus status) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        this.username = username;
-        this.status = status;
-    }
 }

9c2f82505b915c5ed190897823894aeca84a3b54
bhnone <>
2025-01-01 13:51:02 +0900

    l17 JPA - Specification API dynamic query

---
+++ src/main/java/com/ra/repository/specification/SearchOperation.java
@@ -0,0 +1,36 @@
+package com.ra.repository.specification;
+
+public enum SearchOperation {
+    EQUALITY, NEGATION, GREATER_THAN, LESS_THAN, LIKE, STARTS_WITH, ENDS_WITH, CONTAINS;
+
+    public static final String[] SIMPLE_OPERATION_SET = {":", "!", ">", "<", "~"};
+
+    public static final String OR_PREDICATE_FLAG = "'";
+
+    public static final String ZERO_OR_MORE_REGEX = "*";
+
+    public static final String OR_OPERATOR = "or";
+
+    public static final String AND_OPERATOR = "and";
+
+    public static final String LEFT_PARANTHESIS = "(";
+
+    public static final String RIGHT_PARANTHESIS = ")";
+
+    public static SearchOperation getSimpleOperation(final char input) {
+        switch (input) {
+            case ':':
+                return EQUALITY;
+            case '!':
+                return NEGATION;
+            case '>':
+                return GREATER_THAN;
+            case '<':
+                return LESS_THAN;
+            case '~':
+                return LIKE;
+            default:
+                return null;
+        }
+    }
+}

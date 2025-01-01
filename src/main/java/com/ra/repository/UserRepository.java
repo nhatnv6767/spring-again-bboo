c0b824571cb588d471a68c6dbfc4f98a0abb738d
bhnone <>
2025-01-01 16:38:17 +0900

    l18 JPA Query return List

--- src/main/java/com/ra/repository/UserRepository.java
+++ src/main/java/com/ra/repository/UserRepository.java
@@ -3,8 +3,15 @@ package com.ra.repository;
 import com.ra.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    // distinct
+    @Query(value = "SELECT DISTINCT FROM User u where u.firstName =:firstName and u.lastName =:lastName")
+    List<User> findDistinctByFirstNameAndLastName();
 }

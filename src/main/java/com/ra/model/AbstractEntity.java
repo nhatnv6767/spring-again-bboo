459e3974acc4803af3a09f0b533da429ae7676f4
bhnone <>
2025-01-02 00:30:37 +0900

    l2. API Security model

--- src/main/java/com/ra/model/AbstractEntity.java
+++ src/main/java/com/ra/model/AbstractEntity.java
@@ -6,16 +6,17 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
-public abstract class AbstractEntity {
+public abstract class AbstractEntity<T extends Serializable> implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private T id;
 
     @Column(name = "created_at")
     @CreationTimestamp

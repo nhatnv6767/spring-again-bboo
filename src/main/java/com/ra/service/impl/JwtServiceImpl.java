bdc1ccb950ddca741bb24534fd136855f6fe7aad
bhnone <>
2025-01-02 15:13:41 +0900

    l4. Spring Security _ AccessToken and Refresh token: isValid token

--- src/main/java/com/ra/service/impl/JwtServiceImpl.java
+++ src/main/java/com/ra/service/impl/JwtServiceImpl.java
@@ -37,8 +37,13 @@ public class JwtServiceImpl implements JwtService {
     }
 
     @Override
-    public boolean isValid(String token, UserDetails user) {
-        return false;
+    public boolean isValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
     }
 
     private String generateToken(Map<String, Object> claims, UserDetails userDetails) {

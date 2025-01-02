12ac51269146a7d633c57376b54205bee3ecdd7a
bhnone <>
2025-01-02 17:12:09 +0900

    l5 API security - Refresh token & logout: using refresh token

--- src/main/java/com/ra/controller/AuthenticationController.java
+++ src/main/java/com/ra/controller/AuthenticationController.java
@@ -4,16 +4,14 @@ import com.ra.dto.request.SignInRequest;
 import com.ra.dto.response.SignInResponse;
 import com.ra.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Validated
@@ -28,5 +26,10 @@ public class AuthenticationController {
     @PostMapping("/access")
     public ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest request) {
         return new ResponseEntity<>(authenticationService.signIn(request), HttpStatus.OK);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<SignInResponse> refresh(HttpServletRequest request) {
+        return new ResponseEntity<>(authenticationService.refresh(request), HttpStatus.OK);
     }
 }
--- src/main/java/com/ra/service/AuthenticationService.java
+++ src/main/java/com/ra/service/AuthenticationService.java
@@ -2,7 +2,9 @@ package com.ra.service;
 
 import com.ra.dto.request.SignInRequest;
 import com.ra.dto.response.SignInResponse;
+import com.ra.model.User;
 import com.ra.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +14,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -51,5 +56,29 @@ public class AuthenticationService {
             log.error("Authentication failed for user: {}", request.getUsername(), e);
             throw new BadCredentialsException("Invalid username or password");
         }
+    }
+
+    public SignInResponse refresh(HttpServletRequest request) {
+//        System.out.println(request.getHeader("x-token"));
+        String refreshToken = request.getHeader("x-token");
+        if (!StringUtils.hasLength(refreshToken)) {
+            throw new BadCredentialsException("Invalid token");
+        }
+        // extract username from token
+        String userName = jwtService.extractUsername(refreshToken);
+        // check it into database
+        Optional<User> user = userRepository.findByUsername(userName);
+        if (!jwtService.isValid(refreshToken, user.get())) { // get() because User is optional
+            throw new BadCredentialsException("Invalid token");
+        }
+        String accessToken = jwtService.generateToken(user.get());
+
+        return SignInResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .userId(user.get().getId())
+                .phoneNumber(user.get().getPhone())
+                .role(user.get().getType().name())
+                .build();
     }
 }
